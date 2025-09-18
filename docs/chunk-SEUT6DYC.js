4c66b167e9c6d4984e1291a5d6278976371a2c10
youssef7070 <youssefmo7amed7070@gamil.com>
2025-09-18 03:20:18 +0300

    Fix GitHub Pages 404 routing issue

---
+++ docs/chunk-SEUT6DYC.js
@@ -0,0 +1,1 @@
+import { Za as t } from "./chunk-TYF7JRNT.js"; import "./chunk-4CLCTAJ7.js"; var a = class e { static \u0275fac = function (n) { return new (n || e) }; static \u0275cmp = t({ type: e, selectors: [["app-brands"]], decls: 0, vars: 0, template: function (n, s) { }, encapsulation: 2 }) }; export { a as BrandsComponent };
